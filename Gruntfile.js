7b79b4ebb4391055190bd74f97a78dcabacf7915
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-01-25 12:40:46 -0800

    Switch to using `esbuild` (#2)
    * Updated Gruntfile to output to dist folder
    * Add new 'index.scss' that combines the CSS and SCSS together
    * Build with esbuild and output both non-minified stylesheet and minimized versions by default
    * Remove old/deprecated version of generated CSS
    * Add eslint support and reformat scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,99 +13,99 @@ https://github.com/kepano/obsidian-minimal
 */
 
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        /* Get the user-defined OBSIDIAN_PATH from .env file
+    /* Get the user-defined OBSIDIAN_PATH from .env file
            so that we can live reload the theme in the vault */
-        env: {
-            local: {
-                src: ".env"
-            }
-        },
+    env: {
+      local: {
+        src: '.env'
+      }
+    },
 
-        /* Compile the compressed and uncompressed versions of
+    /* Compile the compressed and uncompressed versions of
         the theme using Sass */
-        sass: {
-            unminified: {
-                options: {
-                    /* sourcemap: 'none' */
-                },
-                files: {
-                    'src/css/main.css': 'src/scss/index.scss'
-                }
-            },
-            dist: {
-                options: {
-                    style: 'compressed',
-                    /* sourcemap: 'none' */
-                },
-                files: {
-                    'src/css/main.min.css': 'src/scss/index.scss'
-                }
-            }
+    sass: {
+      unminified: {
+        options: {
+          /* sourcemap: 'none' */
         },
-
-        /* Minify theme used for distribution and live reload */
-        cssmin: {
-            options: {
-                advanced: false,
-                aggressiveMerging: false,
-                mediaMerging: false,
-                restructuring: false
-            },
-            target: {
-                files: {
-                    'src/css/main.min.css': 'src/css/main.min.css'
-                }
-            }
-        },
-
-        /* Concatenate theme files adding in the commented license, plugin compatibility,
-           and Style Settings that would otherwise be removed in compression */
-        concat_css: {
-            dist: {
-                files: {
-                    'theme.css': ['src/css/license.css', 'src/css/main.min.css', 'src/css/plugin-compatibility.css', 'src/css/style-settings.css']
-                }
-            },
-            unminified: {
-                files: {
-                    'Minimal.css': ['src/css/license.css', 'src/css/main.css', 'src/css/plugin-compatibility.css', 'src/css/style-settings.css']
-                }
-            }
-        },
-
-        /* Copy the finished distribution file from the working directory to the vault
-        directory and use correct theme name */
-        copy: {
-            local: {
-                expand: true,
-                src: 'theme.css',
-                dest: process.env.OBSIDIAN_PATH,
-                rename: function (dest, src) {
-                    return dest + 'theme.css';
-                }
-            }
-        },
-
-        /* Watch for changes, and compile new changes */
-        watch: {
-            css: {
-                files: ['src/**/*.scss', 'src/**/*.css'],
-                tasks: ['env', 'sass:unminified', 'sass:dist', 'cssmin', 'concat_css', 'copy',]
-            }
+        files: {
+          'dist/main.css': 'src/scss/index.scss'
         }
-    });
-    grunt.loadNpmTasks('grunt-env');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-concat-css');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('loadconst', 'Load constants', function () {
-        grunt.config('OBSIDIAN_PATH', process.env.OBSIDIAN_PATH);
-    });
-    grunt.registerTask('default', ['env:local', 'loadconst', 'env', 'sass:unminified', 'sass:dist', 'cssmin', 'concat_css', 'copy']);
-    grunt.registerTask('watch', ['env:local', 'loadconst', 'watch']);
-};
+      },
+      dist: {
+        options: {
+          style: 'compressed'
+          /* sourcemap: 'none' */
+        },
+        files: {
+          'dist/main.min.css': 'src/scss/index.scss'
+        }
+      }
+    },
+
+    /* Minify theme used for distribution and live reload */
+    cssmin: {
+      options: {
+        advanced: false,
+        aggressiveMerging: false,
+        mediaMerging: false,
+        restructuring: false
+      },
+      target: {
+        files: {
+          'dist/main.min.css': 'dist/main.min.css'
+        }
+      }
+    },
+
+    /* Concatenate theme files adding in the commented license, plugin compatibility,
+           and Style Settings that would otherwise be removed in compression */
+    concat_css: {
+      dist: {
+        files: {
+          'dist/theme.css': ['src/css/license.css', 'dist/main.min.css', 'src/css/plugin-compatibility.css', 'src/css/style-settings.css']
+        }
+      },
+      unminified: {
+        files: {
+          'dist/Minimal.css': ['src/css/license.css', 'dist/main.css', 'src/css/plugin-compatibility.css', 'src/css/style-settings.css']
+        }
+      }
+    },
+
+    /* Copy the finished distribution file from the working directory to the vault
+        directory and use correct theme name */
+    copy: {
+      local: {
+        expand: true,
+        src: 'dist/theme.css',
+        dest: process.env.OBSIDIAN_PATH,
+        rename: function (dest, src) {
+          return dest + 'theme.css'
+        }
+      }
+    },
+
+    /* Watch for changes, and compile new changes */
+    watch: {
+      css: {
+        files: ['src/**/*.scss', 'src/**/*.css'],
+        tasks: ['env', 'sass:unminified', 'sass:dist', 'cssmin', 'concat_css', 'copy']
+      }
+    }
+  })
+  grunt.loadNpmTasks('grunt-env')
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-concat-css')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.registerTask('loadconst', 'Load constants', function () {
+    grunt.config('OBSIDIAN_PATH', process.env.OBSIDIAN_PATH)
+  })
+  grunt.registerTask('default', ['env:local', 'loadconst', 'env', 'sass:unminified', 'sass:dist', 'cssmin', 'concat_css', 'copy'])
+  grunt.registerTask('watch', ['env:local', 'loadconst', 'watch'])
+}
